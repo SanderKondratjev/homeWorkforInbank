6ffa4617d9db6e3c122dda624e9542a1c811aae3
Sander Kondratjev <sanderko@hotmail.com>
2021-05-15 18:30:42 +0300

    Loan Decision Code 2

--- src/main/java/ee/forInbank/homeWork/loanDecisionController/LoanDecisionController.java
+++ src/main/java/ee/forInbank/homeWork/loanDecisionController/LoanDecisionController.java
@@ -14,11 +14,15 @@ public class LoanDecisionController {
     @Autowired
     private LoanDecisionService loanDecisionService;
 
-//    http://localhost:8080/loandecision?customerId=49002010998&loanAmount=4000&loanPeriod=12
+//    TODO testcase 1: http://localhost:8080/loandecision?customerid=49002010965&loanamount=6000&loanperiod=12
+//    TODO testcase 2: http://localhost:8080/loandecision?customerid=49002010976&loanamount=6000&loanperiod=12
+//    TODO testcase 3: http://localhost:8080/loandecision?customerid=49002010987&loanamount=6000&loanperiod=12
+//    TODO testcase 4: http://localhost:8080/loandecision?customerid=49002010998&loanamount=6000&loanperiod=12
+
     @GetMapping("loandecision")
     public String loanSum(@RequestParam("customerid") Double customerId,
                           @RequestParam("loanamount") Double loanAmount,
-                          @RequestParam("loanperiod") Double loanPeriod) {
+                          @RequestParam("loanperiod") Integer loanPeriod) {
         return loanDecisionService.loanSum(customerId, loanAmount, loanPeriod);
     }
 }
--- src/main/java/ee/forInbank/homeWork/loanDecisionService/LoanDecisionService.java
+++ src/main/java/ee/forInbank/homeWork/loanDecisionService/LoanDecisionService.java
@@ -10,18 +10,32 @@ public class LoanDecisionService {
     @Autowired
     private LoanDecisionRepository loanDecisionRepository;
 
-    public String loanSum(Double customerId, Double loanAmount, Double loanPeriod) {
+    public String loanSum(Double customerId, Double loanAmount, Integer loanPeriod) {
         if (loanAmount >= 2000 && loanAmount <= 10000 && loanPeriod >= 12 && loanPeriod <= 60) {
             Double creditModifier = loanDecisionRepository.getCreditModifier(customerId);
             double creditScore;
             creditScore = (creditModifier / loanAmount) * loanPeriod;
-            if (creditScore > 1) {
-
+            if (creditScore < 1) {
+                Double maxLoanAmount = creditModifier * loanPeriod;
+                Double maxLoanPeriod = loanPeriod / creditScore;
+                if (maxLoanAmount >= 2000 && maxLoanAmount <= 10000) {
+                    return "Apologies, the maximum amount you can request is " +
+                            maxLoanAmount + " EUR.";
+                } else if (maxLoanPeriod >= 12 && maxLoanPeriod <= 60) {
+                    return "Apologies, no suitable loan amount was found. The maximum loan period for that amount would be "
+                            + Math.round(maxLoanPeriod) + " months.";
+                } else {
+                    return "Apologies, Your loan application is denied";
+                }
+            } else {
+                Double maxLoanAmount = creditModifier * loanPeriod;
+                if (maxLoanAmount >= 10000) {
+                    return "Your loan application is positive. The maximum amount you can request is 10000 EUR";
+                } else {
+                    return "Your loan application is positive. The maximum amount you can request is " + maxLoanAmount;
+                }
             }
-        } else {
-            return "Please correct data";
         }
-
-
+        return "Please correct data";
     }
 }
