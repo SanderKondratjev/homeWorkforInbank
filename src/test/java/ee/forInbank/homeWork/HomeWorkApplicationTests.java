97a07b760a1b3f72e4673d8a10c9af816d3d39e3
Sander Kondratjev <sanderko@hotmail.com>
2021-05-16 13:24:01 +0300

    Testing

--- src/test/java/ee/forInbank/homeWork/HomeWorkApplicationTests.java
+++ src/test/java/ee/forInbank/homeWork/HomeWorkApplicationTests.java
@@ -1,13 +1,30 @@
 package ee.forInbank.homeWork;
 
+import ee.forInbank.homeWork.loanDecisionController.LoanDecisionController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class HomeWorkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Autowired
+	private LoanDecisionController loanDecisionController;
+
+	@Test
+	void setLoanDecisionController() throws Exception{
+
+		this.mockMvc.perform(get("loan-decision"))
+				.contentType("application/json")
+				.param("customerid", "loanamount", "loanperiod")
+				.andExpect(status().isOk())
+				.andExpect(content().string(containstString("st")));
+	}
 }
